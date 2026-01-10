51702c8bfa38856cfb7a18d731e997fce477a723
Adam <adamyork@users.noreply.github.com>
2026-01-10 07:36:15 -0500

    fix: use resource router to avoid manual routing of static assets

--- src/main/kotlin/com/github/adamyork/sparrow/web/WebConfig.kt
+++ src/main/kotlin/com/github/adamyork/sparrow/web/WebConfig.kt
@@ -8,6 +8,9 @@ import org.springframework.core.io.ClassPathResource
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 
 @Configuration
@@ -22,97 +25,17 @@ class WebConfig : WebFluxConfigurer {
         PhysicsSettingsHandler(physicsSettingsService)
 
     @Bean
-    fun rootRouter() =
-        router {
-            val index = ClassPathResource("static/index.html")
-            val extensions = listOf("js", "css", "wav", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains) or
-                    path("/form.html"))
-            resource(spaPredicate, index)
-        }
+    fun staticResources(): RouterFunction<ServerResponse> =
+        RouterFunctions.resources("/**", ClassPathResource("static/"))
 
     @Bean
-    fun formRouter() =
+    fun spaFallback(): RouterFunction<ServerResponse> =
         router {
-            val index = ClassPathResource("static/form.html")
-            val extensions = listOf("js", "css", "wav", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains)) and
-                    path("/form.html")
-            resource(spaPredicate, index)
-        }
-
-    @Bean
-    fun jsRouter() =
-        router {
-            val index = ClassPathResource("static/main.js")
-            val extensions = listOf("css", "wav", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains) or
-                    path("/form.js"))
-            resource(spaPredicate, index)
-        }
-
-    @Bean
-    fun formJsRouter() =
-        router {
-            val index = ClassPathResource("static/form.js")
-            val extensions = listOf("css", "wav", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains)) and
-                    path("/form.js")
-            resource(spaPredicate, index)
-        }
-
-    @Bean
-    fun cssRouter() =
-        router {
-            val index = ClassPathResource("static/main.css")
-            val extensions = listOf("js", "wav", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains) or
-                    path("/form.css"))
-            resource(spaPredicate, index)
-        }
-
-    @Bean
-    fun formCssRouter() =
-        router {
-            val index = ClassPathResource("static/form.css")
-            val extensions = listOf("js", "wav", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains)) and
-                    path("/form.css")
-            resource(spaPredicate, index)
-        }
-
-    @Bean
-    fun wavRouter() =
-        router {
-            val index = ClassPathResource("game/level-1-music.wav")
-            val extensions = listOf("js", "css", "html", "png")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains))
-            resource(spaPredicate, index)
-        }
-
-    @Bean
-    fun pngRouter() =
-        router {
-            val index = ClassPathResource("static/splash.png")
-            val extensions = listOf("js", "css", "html", "wav")
-            val spaPredicate = !(path("/score") or
-                    path("/physics") or
-                    pathExtension(extensions::contains))
-            resource(spaPredicate, index)
+            GET("/") {
+                ServerResponse
+                    .ok()
+                    .bodyValue(ClassPathResource("static/index.html"))
+            }
         }
 
     @Bean
